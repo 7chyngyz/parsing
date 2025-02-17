e8c26e9b7d13be1072fbf6569734112eac3a3816
7chyngyz <sultanmuratovchyngyz@gmail.com7>
2025-02-17 23:35:43 +0600

    parser 50% ready

--- src/app/file-uploader/file-uploader.component.ts
+++ src/app/file-uploader/file-uploader.component.ts
@@ -12,7 +12,7 @@ import * as Papa from 'papaparse';
 export class CsvUploadComponent {
   csvData: any[] = [];
   protected file: any;
-  selectedRow: any = null; 
+  selectedRows: any[] = [];
 
   constructor() {}
 
@@ -46,37 +46,89 @@ export class CsvUploadComponent {
     return Object.keys(obj);
   }
 
-  downloadSelectedTable() {
-    if (!this.selectedRow) {
-      alert('Please select a row to download');
+  downloadAllTable() {
+    if (this.csvData.length === 0) {
+      alert('No data available to download');
       return;
     }
   
-    const headers = this.getObjectKeys(this.selectedRow);
-    const name = this.selectedRow['OwnerName'] || 'Неизвестный';
-    const email = this.selectedRow['Username'] || 'Не указан';
-  
+    const headers = this.getObjectKeys(this.csvData[0]);
     let content = `<html><body>`;
-    content += `<p>Здравствуйте - <b>${name}</b>,</p>`;
-    content += `<p>Его email: <b>${email}</b></p>`;
-    content += `<p><b>Остальные данные:</b></p><ul>`;
   
-    headers.forEach(header => {
-      if (header !== 'OwnerName' && header !== 'Username') {
-        content += `<li><b>${header}:</b> ${this.selectedRow[header]}</li>`;
-      }
+    this.csvData.forEach(row => {
+      const name = row['OwnerName'] || 'Неизвестный';
+      const email = row['Username'] || 'Не указан';
+  
+      content += `<p>Здравствуйте - <b>${name}</b>,</p>`;
+      content += `<p>Его email: <b>${email}</b></p>`;
+      content += `<p><b>Остальные данные:</b></p><ul>`;
+  
+      headers.forEach(header => {
+        if (header !== 'OwnerName' && header !== 'Username') {
+          content += `<li><b>${header}:</b> ${row[header]}</li>`;
+        }
+      });
+      content += `</ul><hr/>`; 
     });
   
-    content += `</ul></body></html>`;
+    content += `</body></html>`;
   
     const blob = new Blob(["\ufeff" + content], { type: 'application/msword;charset=utf-8' });
     const link = document.createElement('a');
     link.href = URL.createObjectURL(blob);
-    link.download = 'selected-user.doc';
+    link.download = 'all-users.doc'; 
+    link.click();
+  }  
+  
+  downloadSelectedTable() {
+    if (this.selectedRows.length === 0) {
+      alert('Please select at least one row to download');
+      return;
+    }
+  
+    const headers = this.getObjectKeys(this.selectedRows[0]);
+    let content = `<html><body>`;
+  
+    this.selectedRows.forEach(row => {
+      const name = row['OwnerName'] || 'Неизвестный';
+      const email = row['Username'] || 'Не указан';
+  
+      content += `<p>Здравствуйте - <b>${name}</b>,</p>`;
+      content += `<p>Его email: <b>${email}</b></p>`;
+      content += `<p><b>Остальные данные:</b></p><ul>`;
+  
+      headers.forEach(header => {
+        if (header !== 'OwnerName' && header !== 'Username') {
+          content += `<li><b>${header}:</b> ${row[header]}</li>`;
+        }
+      });
+      content += `</ul><hr/>`; 
+    });
+  
+    content += `</body></html>`;
+  
+    const blob = new Blob(["\ufeff" + content], { type: 'application/msword;charset=utf-8' });
+    const link = document.createElement('a');
+    link.href = URL.createObjectURL(blob);
+    link.download = 'selected-users.doc';
     link.click();
   }
   
-  selectRow(index: number) {
-    this.selectedRow = this.csvData[index];
+
+  toggleSelection(row: any) {
+    const index = this.selectedRows.indexOf(row);
+    if (index === -1) {
+      this.selectedRows.push(row);
+    } else {
+      this.selectedRows.splice(index, 1);
+    }
+  }
+
+  toggleAllSelection(event: any) {
+    if (event.target.checked) {
+      this.selectedRows = [...this.csvData];
+    } else {
+      this.selectedRows = [];
+    }
   }
 }
